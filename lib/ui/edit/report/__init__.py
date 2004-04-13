156fcda9cd605fc87a93525b1ae7992ad873cbab
Dan Chudnov <dchud@umich.edu>
2004-04-13 13:54:43 +0000

    Changing Report to Study

--- lib/ui/edit/report/__init__.py
+++ lib/ui/edit/report/__init__.py
@@ -1,20 +1,21 @@
-_q_exports = ['_q_index',
-              ]
+_q_exports = [
+    '_q_index',
+    ]
 
 from quixote.errors import TraversalError
 
-from canary.ui.edit.report import report_ui
-from canary.ui.edit.report.report_ui import ReportActions
+from canary.ui.edit.study import study_ui
+from canary.ui.edit.study.study_ui import StudyActions
 from canary.ui.pages import not_found
 
-_q_index = report_ui._q_index
+_q_index = study_ui._q_index
 
 
 def _q_lookup (request, queued_record_id):
-    #print 'edit.report.__init__._q_lookup(%s)' % record_id
+    #print 'edit.study.__init__._q_lookup(%s)' % record_id
     try:
         if not queued_record_id == None:
-            return ReportActions(queued_record_id)
+            return StudyActions(queued_record_id)
         else:
             raise TraversalError
     except:
