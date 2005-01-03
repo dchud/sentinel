11d7d5b2bac19fdfbe9d1cc22d8c8ec870ee3566
Dan Chudnov <dchud@umich.edu>
2005-01-03 21:38:08 +0000

    Minor cruft removal

--- bin/scgi_handler.py
+++ bin/scgi_handler.py
@@ -11,9 +11,6 @@ from canary.qx_defs import MySessionPublisher
 
 class MyHandler (QuixoteHandler):
 
-    #def __init__ (self, *args, **kwargs):
-    #    QuixoteHandler.__init__(self, *args, **kwargs)
-
     publisher_class = MySessionPublisher
     root_namespace = "canary.ui"
     prefix = ""
