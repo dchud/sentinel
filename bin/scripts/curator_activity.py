0268920015f7675780c0740df57598c6f48b73c2
Dan Chudnov <dchud@umich.edu>
2005-10-03 16:09:50 +0000

    Tweaks to stats module and activity reporter

--- bin/scripts/curator_activity.py
+++ bin/scripts/curator_activity.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python
 
+# $Id$
+
+from datetime import datetime, timedelta
+
 from canary.cmdline import CommandLine
 from canary.context import Context, CanaryConfig
-from canary.report import CuratorActivity
+from canary.loader import QueuedRecord
+from canary.search import RecordSearcher
+from canary.stats import *
+from canary.study import Study
 
-cmdline = CommandLine()
-cmdline.add_option( '--html', dest='html', action='store_true', default=False, 
-    help='output as html' )
-cmdline.parse_args()
 
-context = cmdline.context()
-report = CuratorActivity(context)
 
-if cmdline.html:
-    print report.html()
-else:
-    print report.text()
-
+if __name__ == '__main__':
+    cmdline = CommandLine()
+    cmdline.parse_args()
+    context = cmdline.context()
+    
+    collector = StatCollector(context)
+    collector.add_handlers(
+        CuratorHandler(), 
+        ArticleTypeHandler(), 
+        ExposureHandler(), 
+        OutcomeHandler(), 
+        RiskFactorHandler(), 
+        SpeciesHandler(), 
+        LocationHandler(), 
+        MethodologyTypeHandler(),
+        )
+    
+    searcher = RecordSearcher(context)
+    records = searcher.search('record-status:%i' % QueuedRecord.STATUS_CURATED)
+    
+    today = datetime.now()
+    one_weeks_records = [rec for rec in records 
+        if today - Study(context, rec.study_id).date_modified <= timedelta(7)]
+    collector.process(one_weeks_records)
+    
+    for handler in collector.handlers:
+        print handler.__class__, handler.stats
+    
