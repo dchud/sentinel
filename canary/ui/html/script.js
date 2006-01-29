dc52814d390e50aac1fe6b49c5b41ed59df1d8bf
Dan Chudnov <dchud@umich.edu>
2006-01-29 21:06:46 +0000

    Tweak properties.

--- canary/ui/html/script.js
+++ canary/ui/html/script.js
@@ -1,89 +1,8 @@
 // $Id$
 
-// Borrowed nearly intact from QuirksMode:
-// http://www.quirksmode.org/index.html?/js/display.html
-
 // FIXME: clean this up, ticket #168
-window.onload = function () {
-    var x = document.getElementsByTagName('div');
-    for (var i=0;i<x.length;i++)
-    {
-        if (x[i].className == 'label')
-            x[i].onclick = clickNav;
-    }
-    closeNav();
-    if (top.setNav)
-        setNav(top.setNav,'currentPage');        
-    
-    // Uncomment to remove debugging info
-    //createLoggingPane(true);
-}
-
-
-function closeNav() {
-    var x = document.getElementsByTagName('div');
-    for (var i=0;i<x.length;i++)
-    {
-        if (x[i].className == 'hideable')
-            x[i].style.display = 'none';
-    }
-}
-
-function clickNav(e) {
-    if (!e) var e = window.event;
-    if (e.target) var tg = e.target;
-    else if (e.srcElement) var tg = e.srcElement;
-    while (tg.nodeName != 'DIV') // Safari GRRRRRRRRRR
-        tg = tg.parentNode;
-    var nextSib = tg.nextSibling;
-    while (nextSib.nodeType != 1)
-        nextSib = nextSib.nextSibling;
-    var nextSibStatus = (nextSib.style.display == 'none') ? 'block' : 'none';
-    nextSib.style.display = nextSibStatus;
-}
-
-function setNav(page, newID)
-{
-    // Added try/catch to avoid IE error dialogs (dchud)
-    try {
-        var test = page.indexOf('#')+1;
-    } catch (e) {
-        ;
-    }
-    if (test)
-        page = page.substring(0,test-1);
-    var x = document.getElementsByTagName('a');
-    var i;
-    for (i=0;i<x.length;i++)
-    {
-        if (x[i].href == page)
-        {
-            x[i].id = newID;
-            break;
-        }
-    }
-    if (i < x.length && newID == 'currentPage')
-    {
-        var parDiv = x[i];
-        while (parDiv.parentNode.tagName == 'DIV')
-        {
-            parDiv = parDiv.parentNode;
-            parDiv.style.display = 'block';
-        }
-    }
-}
-
-function hideItem(id)
-{
-    var item = document.getElementById(id);
-    item.style.display='none';
-}
-
-function showItem(id)
-{
-    var item = document.getElementById(id);
-    item.style.display='block';
-}
+// Uncomment to remove debugging info
+createLoggingPane(true);
 
 // *visible() funcs are suggested code from MochiKit docs
 function toggleVisible(elem) {
@@ -210,6 +129,7 @@ function addUserSet (evt) {
     evt = (evt) ? evt : event;
     var charCode = (evt.charCode) ? evt.charCode :
         ((evt.which) ? evt.which : evt.keyCode);
+    logDebug('charCode: ' + charCode);
     if (charCode == 13 || charCode == 3) {
         var waitimage = getElement('waitimage');
         waitimage.innerHTML = '<img src="/images/wait.gif" alt="wait" />';
